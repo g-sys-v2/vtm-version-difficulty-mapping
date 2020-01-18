81ab0683d1c32280071dabe136f434769b693869
g-sys-v2 <>
2020-01-17 17:35:03 -0800

    added v20 1-cancellation logic

--- vtm_version_difficulty_mapping.py
+++ vtm_version_difficulty_mapping.py
@@ -14,35 +14,55 @@ from math import factorial as fact
 DIE_MAX = 10
 
 
+def c(n, x):
+    return fact(n)/(fact(n-x)*fact(x))
+
+
 def binomial(p, n, x):
     if p == 1:
-        return 1
+        return 1.0
     if x > n:
-        return 0
-    c_nx = fact(n)/(fact(n-x)*fact(x))
-    return c_nx*(p**x)*((1-p)**(n-x))
+        return 0.0
+    return c(n, x)*(p**x)*((1-p)**(n-x))
 
 
 def p_one_or_more(p, n, s):
     result = 0
-    for i in range(s, n+1):
-        result += binomial(p, n, i)
-        if result >= 1:
-            return 1.0
+    for x in range(s, n+1):
+        result += binomial(p, n, x)
     return result
 
 
-def format_result(r):
-    return round(r*100, 2)
+def p_crit_fail(n, s, d, x, y):
+    if n == x:
+        return 1.0
+    if x < s:
+        return 0.0
+    return c(n-x, y)*((1/(d-1))**(y))*((1-(1/(d-1)))**(n-x-y))
 
 
-def p_rs(n, s, d):
+def p_v20(n, s, d):
     if n < s:
         return 0
-    return p_one_or_more(((DIE_MAX-d+1)/DIE_MAX), n, s)
+    #return p_one_or_more(((DIE_MAX-d+1)/DIE_MAX), n, s)
+    result = 0
+    for x in range(s, n+1):
+        inter_result = binomial((DIE_MAX-d+1)/DIE_MAX, n, x)
+        for y in range(x-s+1, n-x+1):
+            if y > n-x:
+                break
+            inter_result *= 1 - p_crit_fail(n, s, d, x, y)
+        result += inter_result
+    return result
 
 
-def get_p_dist_rs():
+def p_e5(n, d):
+    if n < d:
+        return 0
+    return p_one_or_more((1/2), n, d)
+
+
+def get_p_dist_v20():
     p_dist = {}
     for n in range(1, 21):
         if n not in p_dist:
@@ -50,18 +70,12 @@ def get_p_dist_rs():
         for s in range(1, 11):
             if s not in p_dist[n]:
                 p_dist[n][s] = {}
-            for d in range(1, 11):
+            for d in range(2, 11):
                 if d not in p_dist[n][s]:
                     p_dist[n][s][d] = {}
-                result = p_rs(n, s, d)
+                result = p_v20(n, s, d)
                 p_dist[n][s][d] = result
     return p_dist
-
-
-def p_e5(n, d):
-    if n < d:
-        return 0
-    return p_one_or_more((1/2), n, d)
 
 
 def get_p_dist_e5():
@@ -77,21 +91,21 @@ def get_p_dist_e5():
     return p_dist
 
 
-def get_p_map(p_dist_rs, p_dist_e5):
+def get_p_map(p_dist_v20, p_dist_e5):
     p_map = {}
-    for n in p_dist_rs.keys():
+    for n in p_dist_v20.keys():
         if n not in p_map:
             p_map[n] = {}
-        for s in p_dist_rs[n].keys():
+        for s in p_dist_v20[n].keys():
             if s not in p_map[n]:
                 p_map[n][s] = {}
-            for d in p_dist_rs[n][s].keys():
+            for d in p_dist_v20[n][s].keys():
                 if d not in p_map[n][s]:
                     p_map[n][s][d] = {}
                 mapped_d = 1
-                min_diff = abs(p_dist_rs[n][s][d] - p_dist_e5[n][mapped_d])
+                min_diff = abs(p_dist_v20[n][s][d] - p_dist_e5[n][mapped_d])
                 for d_e5 in p_dist_e5[n]:
-                    diff = abs(p_dist_rs[n][s][d] - p_dist_e5[n][d_e5])
+                    diff = abs(p_dist_v20[n][s][d] - p_dist_e5[n][d_e5])
                     if diff < min_diff:
                         min_diff = diff
                         mapped_d = d_e5
@@ -99,9 +113,9 @@ def get_p_map(p_dist_rs, p_dist_e5):
     return p_map
 
 
-p_dist_rs = get_p_dist_rs()
-p_dist_e5 = get_p_dist_e5()
-p_map = get_p_map(p_dist_rs, p_dist_e5)
+def format_result(r):
+    return round(r*100, 2)
+
 
 help_statement = f"description:\n\tConverts a VTM V20 roll into a 5E roll. Returns required 5E " \
                  f"Difficulty for the given number of dice.\n"\
@@ -110,7 +124,11 @@ help_statement = f"description:\n\tConverts a VTM V20 roll into a 5E roll. Retur
 if len(argv) == 1 or argv[1] == "-h" or argv[1] == "h":
     print(help_statement)
     exit()
+
 try:
+    p_dist_v20 = get_p_dist_v20()
+    p_dist_e5 = get_p_dist_e5()
+    p_map = get_p_map(p_dist_v20, p_dist_e5)
     n = int(argv[1])
     s = int(argv[2])
     d = int(argv[3])
@@ -120,7 +138,7 @@ try:
         f"\t\tdice: {n}\n"
         f"\t\trequired successes: {s}\n"
         f"\t\tdifficulty: {d}\n"
-        f"\tsuccess chance: {format_result(p_dist_rs[n][s][d])}%\n"
+        f"\tsuccess chance: {format_result(p_dist_v20[n][s][d])}%\n"
         f"5e\n"
         f"\tdifficulty: {format_result(p_map[n][s][d])}\n"
         f"\tsuccess chance: {format_result(p_dist_e5[n][p_map[n][s][d]])}%\n"
